86f00a2cebc0b68811eeb4aaccfb15900ecf491d
wdxpz <wdxpz@qq.com>
2024-10-11 06:10:06 +0000

    add llm unified interface

---
+++ llms/infer.py
@@ -0,0 +1,18 @@
+from gemini import complete_gemini
+from claude import complete_claude
+from llama import complete_llama
+from mistral import complete_mistral
+from mistral_small import complete_mistral_s
+
+llms_api = {
+    'gemini': complete_gemini,
+    'claude': complete_claude,
+    'llama': complete_llama,
+    'mistral': complete_mistral,
+    'mistral_s': complete_mistral_s
+}
+def llm_infer(llm_name: str, prompt: str):
+    if llm_name not in llms_api.keys():
+        raise Exception(f"not valid LLM model {llm_name}, only support {str(llms_api.keys())}!")
+    result = llms_api[llm_name](prompt)
+    return result
